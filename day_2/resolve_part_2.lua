7a7730c19f5bc68e2e6b601229372d89297a2b42
Sblash <marmi95@gmail.com>
2024-12-04 20:51:19 +0100

    day 2 part 2

--- day_2/resolve_part_2.lua
+++ day_2/resolve_part_2.lua
@@ -40,6 +40,48 @@ function ImplodeTable(values)
 	return str
 end
 
+function SearchForSortChangesToRemove(reportValues)
+	local results = {}
+	results['increment']['count'] = 0
+	results['increment']['values'] = {}
+	results['decrement']['count'] = 0
+	results['decrement']['values'] ={}
+	results['nothing']['count'] = 0
+	results['nothing']['values'] = {}
+
+	for i=1, #reportValues-1 do
+		local key = reportValues[i].."_"..reportValues[i+1]
+        	if (tonumber(reportValues[i]) < tonumber(reportValues[i+1])) then
+			table.insert(results['increment']['values'], key)
+			results["increment"]['count'] = results["increment"]['count'] + 1
+		elseif (tonumber(reportValues[i]) > tonumber(reportValues[i+1])) then
+			table.insert(results['increment']['values'], key)
+			results["decrement"]['count'] = results["decrement"]['count'] + 1
+		else
+			table.insert(results['increment']['values'], key)
+			results["nothing"]['count'] = results["nothing"]['count'] + 1
+		end
+        end
+
+	for k,v in pairs(results) do
+		print("	"..k..": "..tostring(v))
+	end
+
+	if (results['nothing']['count'] > 0) then
+		return Split(results['nothing']['values'][1], "_")[1]
+	end
+
+	if (results['increment']['count'] > 0 and results['decrement']['count'] > 0) then
+		if (results['increment']['count'] < results['decrement']['count']) then 
+			return Split(results['increment']['values'][1], '_')[1]
+		else
+			return Split(results['decrement']['values'][1], '_')[1]
+		end
+	end
+
+        return nil
+end
+
 local file = io.open("input_file.txt", "r")
 local countSafeReport = 0
 
@@ -59,8 +101,21 @@ for line in file:lines() do
 		print('safe: '..ImplodeTable(reportValues))
 		countSafeReport = countSafeReport + 1
 	else
-		print('unsafe: '..ImplodeTable(reportValues))
+		print('candidate unsafe: '..ImplodeTable(reportValues)..' Trying to fix it...')
+		print('Analyzing for sort changes...')
+		local sortChanges = SearchForSortChangesToRemove(reportValues)
+		--local differenceLevelToRemove = 
+		if (sortChanges ~= nil) then
+			--print('unsafe: there are more than 1 sort change, can\t fix it.')
+		--else
+			print('Found 1 sort change, applying the Problem Dampener...')
+		elseif (true) then
+		
+		else
+			print('unsafe')
+		end
 	end
+	print()
 	print()
 end
 
